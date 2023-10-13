62ead95cd043397da815c65f468655a9798ed4a7
DizzyWaters <mkulvanovskis@gmail.com>
2023-10-13 18:19:23 +0300

    some fixes at 2.2 + solution with double

--- LW1_part2-2_solution/LW1_part2-2_project/main.cpp
+++ LW1_part2-2_solution/LW1_part2-2_project/main.cpp
@@ -7,8 +7,10 @@ const float Pi = 3.1416;
 
 int main()
 {
+	// Here is actually working a solution where float was used, but I don't like the percision, so there will be another one with double.
+
+	/*
 	float circleArea, circleRadius;
-	char test;
 	printf("Enter input data:\n\n");
 	printf("Circle radiuss (cm) -> ");
 	scanf_s("%f", &circleRadius);
@@ -16,6 +18,18 @@ int main()
 	circleArea = Pi * (circleRadius * circleRadius);
 	printf("Area of the circle is %.4f sq.cm", circleArea);
 	printf("\n");
-	
+	*/
+
+	// With double
+
+	double circleArea = 0;
+	double circleRadius = 0;
+	printf("Enter input data:\n\n");
+	printf("Circle radiuss (cm) -> ");
+	scanf_s("%lf", &circleRadius);
+	circleArea = Pi * (circleRadius * circleRadius);
+	printf("\nArea of the circle is %.4lf sq.cm", circleArea);
+	printf("\n");
+
 	return 0;
 }
